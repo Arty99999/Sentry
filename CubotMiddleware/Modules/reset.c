4e9b03d931890075caf95286297179a31b9a49d6
Arty99999 <146449772+Arty99999@users.noreply.github.com>
2025-04-18 12:16:02 +0800

    1

--- CubotMiddleware/Modules/reset.c
+++
@@ -1,8 +0,0 @@
-#include "stm32h7xx.h"                  // Device header
-uint8_t SoftResetFlag=0;
-
-void SoftReset(void)					//软件复位
-{	
-	__set_FAULTMASK(1); 				//关闭所有中断 
-	 NVIC_SystemReset(); 				//复位
-}
