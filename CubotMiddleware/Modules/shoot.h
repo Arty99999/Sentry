14e69f77ceea5fceb170521b6eda8b4e21ee5b24
Arty99999 <146449772+Arty99999@users.noreply.github.com>
2025-04-18 12:13:36 +0800

    1

---
+++ CubotMiddleware/Modules/shoot.h
@@ -0,0 +1,52 @@
+#ifndef SHOOT_H
+#define SHOOT_H
+
+#include "stm32h7xx_hal.h"
+#include "motor.h"
+#include "pid.h"
+#include "brain.h"
+typedef struct
+{
+	struct
+	{
+		Motor motor2006;
+		BasePID_Object RunPID;
+		
+		float Delta_Angle;
+		float Plate_Angle;
+		float Target_Angle;
+		float Angle_Sense;
+		float Plate_Out;
+		uint32_t Shoot_Cut;
+		uint16_t Fire_Rate;
+		uint8_t  Fire_Divider;
+		uint8_t  Fire_Margin;
+
+		int16_t ShootNum;
+		uint8_t  Shoot_rest_flag;
+			uint8_t heat_status;
+	}Shoot_Plate;
+	
+	struct
+	{
+		Motor motor3508[2];		
+		BasePID_Object Friction_PID[2];			
+		
+		float Friction_Target_Speed[2];
+		float Friction_Out[2];
+		int16_t Friction_Speed[2];	
+		int16_t Friction_Start;
+	}Friction_Wheel;
+	
+}Ammo_Booster;
+
+float Pid_Control(BasePID_Object* base_pid, float target, int16_t Feedback);
+void AmmoBoosterInit(Ammo_Booster *ammo_booster,BasePID_Object friction_pid, BasePID_Object friction_pid1,BasePID_Object load_pid);
+void AmmoBoosterInit1(Ammo_Booster *ammo_booster2, BasePID_Object friction_pid, BasePID_Object friction_pid1,BasePID_Object load_pid, CanNumber canx);
+void ShootPlateControl(Ammo_Booster *ammo_booster,Brain_t* brain);
+void FrictionWheelControl(Ammo_Booster *ammo_booster);
+
+extern Ammo_Booster AmmoBooster;
+extern uint8_t gun_state;
+extern uint8_t change_gun;
+#endif
